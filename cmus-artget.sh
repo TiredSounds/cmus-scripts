aadd08889d5ae42af59c66d08266d9eed49e0e9f
TiredSounds <tiredsounds17@gmail.com>
2015-11-05 16:10:52 +0000

    Update cmus-artget.sh

--- cmus-artget.sh
+++ cmus-artget.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 ## Download cover art for currently playing album in Cmus music player.
 
+## Requires glyr
+
 ARTIST=$( cmus-remote -Q | grep "tag artist " | sed "s/tag artist //" )
 ALBUM=$( cmus-remote -Q | grep "tag album " | sed "s/tag album //" )
 FOLDER=$( cmus-remote -Q | grep "file" | sed "s/file //" | rev | \
